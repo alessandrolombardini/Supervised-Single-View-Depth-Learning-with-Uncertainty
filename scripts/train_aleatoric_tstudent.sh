4446f02d93e24e47a1fbb26772d20eb051b24d40
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-17 17:06:54 +0100

    More description in deleting file on .sh files

--- scripts/train_aleatoric_tstudent.sh
+++ scripts/train_aleatoric_tstudent.sh
@@ -1,17 +1,20 @@
 # Log script
-if [ -f "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/log/aleatoric_tstudent.out" ]; then
-    rm /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/log/aleatoric_tstudent.out
+if [ -f "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/log/aleatoric.tstudent.out" ]; then
+    echo "Deleting results/log/aleatoric.tstudent.out..."
+    rm /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/log/aleatoric.tstudent.out
 fi
 # Log slurm
-if [ -f "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/slurms/R-training_laplacian.out" ]; then
-    rm /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/slurms/R-training_laplacian.out
+if [ -f "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/slurms/R-training.tstudent.out" ]; then
+    echo "Deleting results/slurms/T-training_tstudent.out..."
+    rm /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/slurms/R-training.tstudent.out
 fi
 # Checkpoint
-if [ -d "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/checkpoints/aleatoric_tstudent" ]; then
-    rm -rf /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/checkpoints/aleatoric_tstudent
+if [ -d "/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/checkpoints/aleatoric.tstudent" ]; then
+    echo "Deleting results/checkpoints/aleatoric.tstudent..."
+    rm -rf /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/checkpoints/aleatoric.tstudent
 fi
 
 cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-python3 main.py --uncertainty "aleatoric_tstudent"
+python3 main.py --uncertainty "aleatoric.tstudent"
