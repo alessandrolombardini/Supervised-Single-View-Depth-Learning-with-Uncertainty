347b4f39d0533eadee4ce2029f8af48fafab0153
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-17 17:26:39 +0100

    Push results of train.sh on a file

--- train.sh
+++ train.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Training Gaussian"
+echo "Training Gaussian"  >> output.txt
 sbatch sbatch_train_gaussian.sh &
 sleep 5m
 
-echo "Training Laplacian"
+echo "Training Laplacian"  >> output.txt
 sbatch sbatch_train_laplacian.sh &
 sleep 5m
 
-echo "Training T-Student"
+echo "Training T-Student"  >> output.txt
 sbatch sbatch_train_tstudent.sh &
 slee 5m 
 
-echo "Training Normal"
+echo "Training Normal"  >> output.txt
 sbatch sbatch_train_normal.sh &
 
