bbcc6a3c87717ecce7838aba4d31bba4040b2a11
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-17 17:36:13 +0100

    Reorganized scripts x2

--- scripts/sbatch_train_laplacian.sh
+++ scripts/sbatch_train_laplacian.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=20G
-#SBATCH --output=../results/slurms/R-%x.out
+#SBATCH --output=results/slurms/R-%x.out
 
-srun --output=../results/log/aleatoric_laplacian.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh train/train_aleatoric_laplacian.sh
+srun --output=results/log/aleatoric_laplacian.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh scripts/train/train_aleatoric_laplacian.sh
 
