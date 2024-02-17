56fd97953d75c49b2a240be722f59b50403db82e
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-17 14:07:43 +0100

    Update .sh files to remove files before execute

--- scripts/train_aleatoric_tstudent.sh
+++ scripts/train_aleatoric_tstudent.sh
@@ -1,4 +1,16 @@
-cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
+# Log script
+path = /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/log/aleatoric_tstudent.out
+if [ -e "$path" ]; then
+    rm -rf "$path"
+# Log slurm
+path = /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/slurms/R-training_laplacian.out
+if [ -e "$path" ]; then
+    rm -rf "$path"
+# Checkpoint
+path = /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/results/checkpoints/aleatoric_tstudent
+if [ -e "$path" ]; then
+    rm -rf "$path"cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
+
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 main.py --uncertainty "aleatoric_tstudent"
