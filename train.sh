8adde88f95c04c72ee6c83c757738f8d7a1c6f8b
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-21 12:55:43 +0100

    Change the script to train

--- train.sh
+++ train.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-#echo "Training Gaussian - CIFAR10"  >> output.txt
-#sbatch scripts/sbatch_train_gaussian_cifar10.sh &
-#sleep 300
-#echo "Training Gaussian - MNIST"  >> output.txt
-#sbatch scripts/sbatch_train_gaussian_mnist.sh &
-#sleep 300
-#echo "Training Gaussian - FASHION MNIST"  >> output.txt
-#sbatch scripts/sbatch_train_gaussian_fashion_mnist.sh &
-#sleep 300
-
+echo "Training Gaussian - FASHION MNIST"  >> output.txt
+sbatch scripts/sbatch_train_gaussian_fashion_mnist.sh &
+sleep 300
 echo "Training T-Student - FASHION MNIST"  >> output.txt
 sbatch scripts/sbatch_train_tstudent_fashion_mnist.sh &
+sleep 300
+echo "Training Laplacian - FASHION MNIST"  >> output.txt
+sbatch scripts/sbatch_train_laplacian_fashion_mnist.sh &
 sleep 300
 echo "Training Normal - FASHION MNIST"  >> output.txt
 sbatch scripts/sbatch_train_normal_fashion_mnist.sh &
 sleep 300
-#echo "Training Laplacian - FASHION MNIST"  >> output.txt
-#sbatch scripts/sbatch_train_laplacian_fashion_mnist.sh &
-#sleep 300
 
-#echo "Training Laplacian - CIFAR10"  >> output.txt
-#sbatch scripts/sbatch_train_laplacian_cifar10.sh &
+#echo "Training Gaussian - CIFAR10"  >> output.txt
+#sbatch scripts/sbatch_train_gaussian_cifar10.sh &
 #sleep 300
 #echo "Training T-Student - CIFAR10"  >> output.txt
 #sbatch scripts/sbatch_train_tstudent_cifar10.sh &
+#sleep 300
+#echo "Training Laplacian - CIFAR10"  >> output.txt
+#sbatch scripts/sbatch_train_laplacian_cifar10.sh &
 #sleep 300
 #echo "Training Normal - CIFAR10"  >> output.txt
 #sbatch scripts/sbatch_train_normal_cifar10.sh &
 #sleep 300
 
-#echo "Training Laplacian - MNIST"  >> output.txt
-#sbatch scripts/sbatch_train_laplacian_mnist.sh &
+#echo "Training Gaussian - MNIST"  >> output.txt
+#sbatch scripts/sbatch_train_gaussian_mnist.sh &
 #sleep 300
 #echo "Training T-Student - MNIST"  >> output.txt
 #sbatch scripts/sbatch_train_tstudent_mnist.sh &
+#sleep 300
+#echo "Training Laplacian - MNIST"  >> output.txt
+#sbatch scripts/sbatch_train_laplacian_mnist.sh &
 #sleep 300
 #echo "Training Normal - MNIST"  >> output.txt
 #sbatch scripts/sbatch_train_normal_mnist.sh &
