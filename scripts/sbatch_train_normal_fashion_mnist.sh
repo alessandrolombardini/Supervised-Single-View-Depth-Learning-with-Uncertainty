a40df075716b8f308c404ee12218de7f6ab29e58
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-02-21 03:02:36 +0100

    Commands for server

--- scripts/sbatch_train_normal_fashion_mnist.sh
+++ scripts/sbatch_train_normal_fashion_mnist.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem-per-cpu=20G
 #SBATCH --output=results/slurms/training_normal_fashion_mnist.out
 
-srun --output=-results/log/normal_fashion_mnist.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh scripts/train/training_normal_fashion_mnist.sh
+srun --output=results/log/normal_fashion_mnist.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh scripts/train/training_normal_fashion_mnist.sh
 
