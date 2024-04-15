0e22a15491d7aec44739e7036703b1489d6d4a62
Alessandro Lombardini Lombardini <alombard@dgx01.i3a.es-enp1s0f0>
2024-04-15 08:18:44 +0000

    Modified train script

--- scripts/sbatch_train_normal_cifar10.sh
+++ scripts/sbatch_train_normal_cifar10.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem-per-cpu=20G
 #SBATCH --output=results/slurms/training_normal_cifar10.out
 
-srun --output=-results/log/normal_cifar10.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh scripts/train/train_normal_cifar10.sh
+srun --output=results/log/normal_cifar10.out --container-mounts=/raid/ropert/alombard:/workspace --container-workdir=/workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty --container-image=nvcr.io#nvidia/pytorch:23.09-py3 sh scripts/train/train_normal_cifar10.sh
 
