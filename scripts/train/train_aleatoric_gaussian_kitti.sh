f16856806b6600f63f313e8bebf9bd872014ca9e
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-04-16 22:53:51 +0200

    Change of call to the main in script

--- scripts/train/train_aleatoric_gaussian_kitti.sh
+++ scripts/train/train_aleatoric_gaussian_kitti.sh
@@ -17,4 +17,5 @@
 cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-python3 main.py --uncertainty "aleatoric.gaussian" --data_name "kitti"
+#python3 main.py --uncertainty "aleatoric.gaussian" --data_name "kitti"
+python3 main.py
