115ee65074248302e7517fc55aa61e250b958ed5
Alessandro Lombardini <alessandro.lombardini@maggioli.it>
2024-04-17 10:51:59 +0200

    Defined the scripts to compute all the uncertainties x2

--- scripts/train/train_aleatoric_laplacian_kitti.sh
+++ scripts/train/train_aleatoric_laplacian_kitti.sh
@@ -17,4 +17,4 @@
 cd  /workspace/Supervised-Single-View-Depth-Learning-with-Uncertainty/src
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-python3 main.py --uncertainty "aleatoric.laplacian" --data_name "fashion_mnist"
+python3 main.py --uncertainty "aleatoric.laplacian"
